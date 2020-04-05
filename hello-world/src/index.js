267004085be123d1b78d999a167125f7ef443ae8
Akash B <akash.b@zycus.com>
2020-04-05 19:24:45 +0530

    small change for portals

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ProtalRoot from './component/PortalRoot';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
