0547fa761a499b6517551e7d328269ff708a1ebb
AkashB <akash.b@zycus.com>
2020-04-15 15:33:41 +0530

    space resolve

--- react-redux/src/components/CakeComponent.js
+++ react-redux/src/components/CakeComponent.js
@@ -6,7 +6,6 @@ function CakeComponent(props) {
     return (
         <div>
             <h2>Number of Cakes available: {props.numOfCakes}</h2>
-
             <button onClick={props.buyCake}>Buy Cake Now!</button>
         </div>
     )
